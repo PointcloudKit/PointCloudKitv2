eed668c44dd91091ec69a56d87a7febe2aab081a
alexandre camilleri <alexcamill@gmail.com>
2021-03-24 09:46:49 +0800

    Initial Commit

---
+++ ARDemoApp/ARDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARDemoAppApp.swift
+//  ARDemoApp
+//
+//  Created by Alexandre Camilleri on 24/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ARDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
