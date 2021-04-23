cbf4aabc9b6f76726bbb552c89d086ebb9b77ba6
alexandre camilleri <alexcamill@gmail.com>
2021-04-23 10:59:17 +0800

    Remove nested warning

--- CaptureViewer/Sources/CaptureViewer/ProcessorParameters.swift
+++ CaptureViewer/Sources/CaptureViewer/ProcessorParameters.swift
@@ -8,25 +8,29 @@
 import Foundation
 
 public struct ProcessorParameters: Codable {
-    public struct VoxelDownSampling: Codable {
-        public var voxelSize: Double = 0.02
-    }
-    public struct OutlierRemoval: Codable {
-        public struct Statistical: Codable {
-            public var neighbors: Int = 20
-            public var stdRatio: Double = 2.0
-        }
-        public struct Radius: Codable {
-            public var pointsCount: Int = 16
-            public var radius: Double = 0.05
-        }
-
-        public var statistical = Statistical()
-        public var radius = Radius()
-    }
-
     public var voxelDownSampling = VoxelDownSampling()
     public var outlierRemoval = OutlierRemoval()
 
     public init() {}
+}
+
+extension ProcessorParameters {
+    public struct VoxelDownSampling: Codable {
+        public var voxelSize: Double = 0.02
+    }
+    public struct OutlierRemoval: Codable {
+        public var statistical = Statistical()
+        public var radius = Radius()
+    }
+}
+
+extension ProcessorParameters.OutlierRemoval {
+    public struct Statistical: Codable {
+        public var neighbors: Int = 20
+        public var stdRatio: Double = 2.0
+    }
+    public struct Radius: Codable {
+        public var pointsCount: Int = 16
+        public var radius: Double = 0.05
+    }
 }
